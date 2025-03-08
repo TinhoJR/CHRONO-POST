258597c0b814ca15f1dd9d61e211384965db4b1d
tinho-jr <michealugwu16@gmail.com>
2025-03-08 01:20:39 +0100

    ''

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -8,9 +8,22 @@ $(document).ready(function() {
 
         Search.focus()
 
+      });
+
+    
+     let businessConsignee = $(".business-consignee");
+
+       businessConsignee.on("click", function() {
+
+        $(".colored-div").css("display", "inherit").css("transition", "ease-in 0.6s");
+
+        businessConsignee.css("backgroundColor", "white");
+
         
 
-      })
+       })
+
+       
 
 
 })
